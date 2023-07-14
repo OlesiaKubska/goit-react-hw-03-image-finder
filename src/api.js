fa4a5011f38306d594adeca07fa124900f5c275d
OlesiaKubska <kublesia0908@gmail.com>
2023-07-14 10:24:24 +0200

    add api.js and update App.jsx

---
+++ src/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const API_KEY = '36691988-31241ee49ae4977171e194e7f';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const fetchImages = async (query, page, perPage) => {
+    try {
+        const response = await axios.get(BASE_URL, {
+        params: {
+            key: API_KEY,
+            q: query,
+            page: page,
+            per_page: perPage,
+            image_type: 'photo',
+            orientation: 'horizontal',
+        },
+        });
+
+        return response.data.hits.map(image => ({
+        id: image.id,
+        webformatURL: image.webformatURL,
+        largeImageURL: image.largeImageURL,
+        }));
+    } catch (error) {
+        console.error(error);
+        throw new Error('Failed to fetch images');
+    }
+};
+
+const api = {
+    fetchImages,
+};
+
+export default api;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import api from 'api';
+
+const IMAGES_PER_PAGE = 12;
+
+export class App extends Component {
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    try {
+      const images = await api.fetchImages('nature', 1, IMAGES_PER_PAGE);
+      console.log(images);
+      // Делайте что-то с полученными изображениями
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        {/* Ваш код компонента */}
+      </div>
+    );
+  }
+}
